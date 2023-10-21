7965a6dc61201872cb2f523665b10269012f3bbd
kokocuk <stas.gluhovskiy5@gmail.com>
2023-10-22 01:59:53 +0700

    Makefile

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main ()
+{
+    int a,b,c,d;
+    cout << "print a and b for plus operation";
+    cin >> a >> b; 
+    c=a+b;
+    cout << c <<"\n";
+}
